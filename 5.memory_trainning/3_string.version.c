30888ac8ff8fd811aa078ce7f8ef8ae74d43ce0f
kmurata <kmurata@samurai-net.co.jp>
2019-01-31 10:58:28 +0900

    今日中に5章終わらせる

--- 5.memory_trainning/3_string.version.c
+++ 5.memory_trainning/3_string.version.c
@@ -19,32 +19,32 @@ int sleep(unsigned long x) {
 
 int main(void) {
   int i, stage;
-  int level;         // 桁数
-  int success = 0;   // 正解数
-  clock_t start, end; // 開始時間、終了時間
+  int level;
+  int success = 0;
+  clock_t start, end;
+  const char ltr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //定数として大文字全てを持っておく　文字コードのアルファベットが連続しているとは限らないため
 
   srand(time(NULL));//乱数の種
 
-  printf("数値記憶トレーニング\n");
+  printf("英字記憶トレーニング\n");
   do {
     printf("挑戦するレベル（%d ~ %d）：", LEVEL_MIN, LEVEL_MAX);
     scanf("%d", &level);
   } while(level < LEVEL_MIN || level > LEVEL_MAX); 
 
-  printf("%d桁の数値を記憶しましょう。\n", level);
+  printf("%d桁の英字を記憶しましょう。\n", level);
 
   start = clock();
   for (stage = 0; stage < MAX_STAGE; stage++) {
-    char no[LEVEL_MAX + 1];      // 記憶する数字の文字列
-    char x[LEVEL_MAX * 2];       // 入力された数字の文字列
+    char mstr[LEVEL_MAX + 1];      // 記憶する文字列 +1はEOSのため
+    char x[LEVEL_MAX * 2];       // 入力された文字列
 
-    no[0] = '1' + rand() % 9;    // 先頭は'1'~'9'
-    for (i = 1; i < level; i++) {
-      no[i] = '0' + rand() % 10; // 先頭以外は'0'~'9'
+    for (i = 0; i < level; i++){
+      mstr[i] = ltr[rand() % strlen(ltr)]; // 取得する文字の位置に乱数を使う
     }
-    no[level] = '\0';            // EOS忘れずに
+    mstr[level] = '\0';   // EOS忘れずに
 
-    printf("%s", no);
+    printf("%s", mstr);
     fflush(stdout);
     sleep(125 * level);
 
@@ -52,7 +52,7 @@ int main(void) {
     fflush(stdout);
     scanf("%s", x);
 
-    if (strcmp(no, x) != 0)  // strcmpは文字列どうしを比較し、一致すれば0を返す
+    if (strcmp(x, mstr) != 0)  // strcmpは文字列どうしを比較し、一致すれば0を返す
       printf("\a間違いです。\n");
     else {    // 2行以上の時は{}必須
       printf("正解です。\n");
