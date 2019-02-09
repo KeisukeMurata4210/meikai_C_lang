4298450d52ab971ee4ea5b55b496734506b47e09
kmurata <kmurata@samurai-net.co.jp>
2019-02-09 18:22:00 +0900

    頭が回らない。中断

---
+++ 6.calendar/11_calendar1.c
@@ -0,0 +1,42 @@
+# include <stdio.h>
+
+/* 各月の日数 */
+int mday[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+/* year年month月day日の曜日を求める */
+int dayofweek(int year, int month, int day) {
+  if (month == 1 || month == 2) {
+    year--;
+    month += 12;
+  }
+  return (year + year/4 - year/100 + year/400 + (13*month + 8)/5 + day) % 7;
+}
+
+/* year年は閏年か？(0-No, 1-Yes) */
+int is_leap(int year) {
+  return year % 4 == 0 && year % 100 != 0 && year % 400 == 0;
+}
+
+/* year年month月の日数（28~31） */
+int monthdays(int year, int month) {
+  if (month-- != 2)//デクリメントはmdayの添字のため
+    return mday[month];
+  return mday[month] + is_leap(year);  // 2月の時は閏月かチェック
+}
+
+/* y年m月のカレンダーを表示 */
+void put_calendar(int y, int m) {
+  int i;
+  int wd = dayofweek(y, m, 1); // 指定された月の1日の曜日
+  int mdays = monthdays(y, m); // 指定された月の日数
+
+  printf(" 日 月 火 水 木 金 土 \n");
+  printf("--------------------\n");
+
+  printf("%*s", 3 * wd, ""); // 1日より左側のスペースを表示
+
+  for (i = 1; i <= mdays; i++) {
+    printf("%3d", i);
+    if (++wd % 7 == 0)
+  }
+}
