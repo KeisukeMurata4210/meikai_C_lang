92afe5b914e1d00616cb7d2e3ec1dd456fceed5a
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-02-11 14:43:29 +0900

    一つずつ理解していく

--- 6.calendar/12_3_month_calendar.c
+++ 6.calendar/12_3_month_calendar.c
@@ -23,17 +23,103 @@ int monthdays(int year, int month) {
   return mday[month] + is_leap(year);
 }
 
+/* y年m月のカレンダーを２次元配列sに格納 */
+/* 
+
+ */
 int make_calendar(int y, int m, char s[7][22]) {
   int i, k;
   int wd = dayofweek(y, m, 1);/* y年m月1日の曜日 */
   int mdays = monthdays(y, m);/* y年m月の日数 */
   char tmp[4];
 
-  sprintf(s[0], "%10d / %02d     ", y, m); // タイトル（年 / 月）
+  sprintf(s[0], "%10d / %02d      ", y, m); // タイトル（年 / 月）
   for (k = 1; k < 7; k++)
-    s[k][0] = '\0'; // 何で１番目にEOS？？
+    s[k][0] = '\0'; // EOSじゃなくて空文字
+  
+  k = 1;
+  sprintf(s[k], "%*s", 3 * wd, "");// 第１日の左側を空欄で埋める
+
+  for (i = 1; i <= mdays; i++){
+    sprintf(tmp, "%3d", i);
+    strcat(s[k], tmp);
+    if (++wd % 7 == 0)
+      k++;
+  }
+
+  if (wd % 7 == 0)
+    k--;
+  else {
+    for (wd %= 7; wd < 7; wd++)
+      strcat(s[k], "   ");
+  }
+  while (++k < 7)
+    sprintf(s[k], "%21s", "");
 }
 
+/* ３次元配列sbufに格納されたカレンダーを横にn個並べて表示 */
+void print(char sbuf[3][7][22], int n) {
+  int i, j;
+
+  for (i = 0; i < n; i++)
+    print("%s  ", sbuf[i][0]); // タイトルを表示
+  putchar('\n');
+  for (i = 0; i < n; i++)
+    printf(" 日 月 火 水 木 金 土   ");
+  putchar('\n');
+  for (i = 0; i < n; i++)
+    printf("----------------------  ");
+  putchar('\n');
+  for (i = 1; i < 7; i++) {
+    for (j = 0; j < n; j++)
+      printf("%s   ", sbuf[j][i]);
+    putchar('\n');
+  }
+  putchar('\n');
+}
+
+/* y1年m1月からy2年m2月までのカレンダーを表示 */
+void put_calendar(int y1, int m1, int y2, int m2) {
+  int y = y1;
+  int m = m1;
+  int n = 0;
+  char sbuf[3][7][22];     // カレンダー文字列のバッファ
+
+  while (y <= y2) {
+    if (y == y2 && m > m2) break;
+    make_calendar(y, m, sbuf[n++]);
+    if (n == 3) {
+      print(sbuf, n);
+      n = 0;
+    }
+    m++;
+    if (m == 13 && y < y2) {
+      y++;
+      m = 1;
+    }
+  }
+  if (n)
+    print(sbuf, n);
+}
+
+int main(void) {
+  int y1, m1, y2, m2;
+
+  printf("カレンダーを表示します。\n");
+  printf("開始年月を入力せよ。\n");
+  printf("年："); scanf("%d", &y1);
+  printf("月："); scanf("%d", &m1);
+
+  printf("終了年月を入力せよ。\n");
+  printf("年："); scanf("%d", &y2);
+  printf("月："); scanf("%d", &m2);
+
+  putchar('\n');
+
+  put_calendar(y1, m1, y2, m2);
+
+  return 0;
+}
 
 
 /*
