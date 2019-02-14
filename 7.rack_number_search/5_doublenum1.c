bdcc728016dff0ad24d760c50b1dc916271c8bd6
kmurata <kmurata@samurai-net.co.jp>
2019-02-14 19:57:02 +0900

    この調子で朝と夜に進めていこう

--- 7.rack_number_search/5_doublenum1.c
+++ 7.rack_number_search/5_doublenum1.c
@@ -4,45 +4,39 @@
 
 # define MAX_STAGE   10
 # define swap(type, x, y)  do {type t = x; x = y; y = t; } while (0)
-/* 
-do-whileは削除できない！！
-・if文内で呼び出した時に、swap(type, x, y);のセミコロンが空文とみなされelse以降に続かなくなる
-・セミコロンを書かなければ正常に動作するが、特殊な書き方を強いるマクロは定義すべきではない
-
-swapと(type, x, y)の間は空けてはいけない！！
-swap　を　(type, x, y)  do {type t = x; x = y; y = t; } while (0) に置換する、というマクロになってしまう。
- */
 
 int main(void) 
 {
   int i, j, stage;
   int dgt[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-  int a[8];
+  int a[10]; // 一個多くする
   double jikan;
   clock_t start, end;
 
   srand(time(NULL));
 
-  printf("欠けている数字を入力してください。\n");
+  printf("ダブっている数字を入力してください。\n");
 
   start = clock();
   for (stage = 0; stage < MAX_STAGE; stage++) {
     int x = rand() % 9; // 0~8
     int no;
+
     i = j = 0;
-    while (i < 9) {
-      if (i != x)
+    while (i < 9) { // ここだけが違う。乱数xと同じ場合さらにもう一度
+      a[j++] = dgt[i];
+      if (i == x)
         a[j++] = dgt[i];
       i++;
     }
 
-    for (i = 7; i > 0; i--) {  // a[]のシャッフルを追加しただけ a[]の添字は0~7
+    for (i = 9; i > 0; i--) {  // シャッフルの個数も増える
       int j = rand() % (i + 1); // 0~i
       if (i != j)
         swap(int, a[i], a[j]);
     }
 
-    for (i = 0; i < 8; i++)
+    for (i = 0; i < 10; i++) // 表示の個数も増える
       printf("%d ", a[i]);
     printf("：");
 
