7ec3570c47a0977413ff4b3e9201bea133a2b30d
kmurata <kmurata@samurai-net.co.jp>
2019-03-14 16:56:20 +0900

    なぜここで空白文字を出力？？タブ文字への変換、の理解が違うのかな

--- 9.file_processing/5_entab.c
+++ 9.file_processing/5_entab.c
@@ -9,6 +9,30 @@ void entab(FILE *src, FILE *dst, int width)
   int count = 0;
   int ntab  = 0;
   int pos   = 1;
+  for ( ; (ch = fgetc(src)) != EOF; pos++) {/* 入力ファイルから一文字取得する。それがEOFになるまで繰り返す。 */
+    if (ch == ' ') {
+      if (pos % width != 0)
+        count++;   /* 空白文字でかつタブの区切りでないとき、空白文字として加算 */
+      else {
+        count = 0;
+        ntab++;   /* 空白文字でかつタブの区切りのとき、タブとして加算 */
+      }
+    } else {
+      for ( ; ntab > 0; --ntab) /* 空白文字でない時点でタブのカウントがあれば、dstに出力 */
+        fputc('\t', dst);
+      if (ch == '\t')               /* 入力自体がタブ文字なら、空白文字のカウントをゼロにする */
+        count = 0;
+      else {
+        for ( ; count > 0; count--)
+          fputc(' ', dst);         /* なぜここで空白文字を出力する？ */
+      }
+      fputc(ch, dst);
+      if (ch == '\n')
+        pos = 0;
+      else if (ch == '\t')
+        pos += width - (pos - 1) % width - 1;
+    }
+  }
 }
 
 int main(int argc, char *argv[])
