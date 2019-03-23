4bf224f8ce22697b8aa9cf729be13706ee3c2c1b
KeisukeMurata4210 <keisuke.murata4210@gmail.com>
2019-03-23 17:20:25 +0900

    これが出来上がれば実質完了だ

---
+++ 10.memorize_English_words/6_strptrary.c
@@ -0,0 +1,15 @@
+/* 文字列の配列を動的に確保（ポインタの配列） */
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int main(void)
+{
+  int num;       /* 文字列の個数 */
+  char **pt;     /* 要素のポインタが指し示すアドレスが、さらに文字配列を指し示している */
+
+  printf("文字列は何個：");
+  scanf("%d", &num);
+
+  pt = 
+}
